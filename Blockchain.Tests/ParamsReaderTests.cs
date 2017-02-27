fcf49fea20627cd5e238d45f9e92f393ebae8ff7
Michael <michaelfiford@gmail.com>
2017-02-27 21:55:15 +0000

    Add parser for multichain params file (params.dat) to allow blockchains to be customised

---
+++ Blockchain.Tests/ParamsReaderTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blockchain.Tests
+{
+    [TestClass]
+    public class ParamsReaderTests
+    {
+        [TestMethod]
+        public void ReadParamsFromFile_FileExists_Success()
+        {
+            const string fileName = "test.txt";
+            try
+            {
+                var NL = Environment.NewLine;
+                var testText = $"something-interesting = 1.2{NL}" +
+                               $" another-thing = false # comment about the thing {NL}" +
+                               $"my-info = 2{NL}" +
+                               $"    asdf = AD43     {NL}" +
+                               $"my-string = something long and interesting{NL}" +
+                               "# commented-value = 2";
+
+                File.WriteAllText(fileName, testText);
+
+                var dict = ParamsReader.ReadParamsFromFile(fileName);
+
+                Assert.AreEqual(1.2, dict["something-interesting"]);
+                Assert.AreEqual(false, dict["another-thing"]);
+                Assert.AreEqual(2, dict["my-info"]);
+                Assert.AreEqual("AD43", dict["asdf"]);
+                Assert.AreEqual("something long and interesting", dict["my-string"]);
+                Assert.IsFalse(dict.ContainsKey("commented-value"));
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ReadParamsFromFile_FileNotExist_Exception()
+        {
+            ParamsReader.ReadParamsFromFile("file2.txt");
+        }
+
+        [TestMethod]
+        public void ParametersToString_Valid_AccurateString()
+        {
+            var myDict = new Dictionary<string, dynamic>
+            {
+                {"my-key", false},
+                {"your-key", "something cool"},
+                {"another-key", 1.2},
+                {"something-cool", 0}
+            };
+
+            var myString = ParamsReader.ParametersToString(myDict);
+
+            var NL = Environment.NewLine;
+            Assert.AreEqual($"my-key = false{NL}" +
+                            $"your-key = something cool{NL}" +
+                            $"another-key = 1.2{NL}" +
+                            $"something-cool = 0", myString);
+        }
+
+        [TestMethod]
+        public void ParametersToFile_Valid_FileOutputValid()
+        {
+            const string fileName = "file3.txt";
+            try
+            {
+                var myDict = new Dictionary<string, dynamic>
+                {
+                    {"my-key", false},
+                    {"your-key", "something cool"},
+                    {"another-key", 1.2},
+                    {"something-cool", 0}
+                };
+
+                ParamsReader.ParametersToFile(fileName, myDict);
+
+                var myString = File.ReadAllText(fileName);
+
+                var NL = Environment.NewLine;
+                Assert.AreEqual($"my-key = false{NL}" +
+                                $"your-key = something cool{NL}" +
+                                $"another-key = 1.2{NL}" +
+                                $"something-cool = 0", myString);
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+    }
+}
---
+++ Blockchain/ParamsReader.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Blockchain
+{
+    public class ParamsReader
+    {
+        public static Dictionary<string, dynamic> ReadParamsFromFile(string fileName)
+        {
+            // File wont be huge, therefore okay to read into memory
+            var fileContents = File.ReadAllText(fileName);
+
+            return ReadParamsFromString(fileContents);
+        }
+
+        public static Dictionary<string, dynamic> ReadParamsFromString(string paramsContent)
+        {
+            var myParams = new Dictionary<string, dynamic>();
+            var x = 0;
+            foreach (var line in paramsContent.Split(new[] {Environment.NewLine}, StringSplitOptions.None))
+            {
+                x++;
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var trimmedLine = line.Trim();
+
+                // Look for comment, ignore everything after '#'
+                var commentIndex = trimmedLine.IndexOf('#');
+                // Line comment
+                if (commentIndex == 0)
+                    continue;
+
+                var interestingPart = commentIndex > -1
+                    ? trimmedLine.Substring(0, commentIndex).Trim()
+                    : trimmedLine;
+                var parts = interestingPart.Split('=');
+
+                if (parts.Length != 2)
+                {
+                    var msg = parts.Length > 2 ? "more than one equals (=)" : "missing equals (=)";
+                    Debug.WriteLine($"Invalid line ({x}), {msg}: \"{interestingPart}\"");
+                    continue;
+                }
+
+                myParams[parts[0].Trim()] = ParseValue(parts[1].Trim());
+            }
+            return myParams;
+        }
+
+        public static string ParametersToString(Dictionary<string, dynamic> dict)
+        {
+            return string.Join(Environment.NewLine, dict.Select(d => $"{d.Key} = {ValueOut(d.Value)}"));
+        }
+
+        public static void ParametersToFile(string fileName, Dictionary<string, dynamic> dict)
+        {
+            var text = ParametersToString(dict);
+            File.WriteAllText(fileName, text);
+        }
+
+        private static string ValueOut(dynamic value)
+        {
+            if (value is bool)
+                return value.ToString().ToLower();
+
+            return value.ToString();
+        }
+
+        private static dynamic ParseValue(string value)
+        {
+            bool b;
+            if (bool.TryParse(value, out b))
+                return b;
+
+            int i;
+            if (int.TryParse(value, out i))
+                return i;
+
+            double d;
+            if (double.TryParse(value, out d))
+                return d;
+
+            // Hex values are stored as strings
+
+            return value;
+        }
+    }
+}
