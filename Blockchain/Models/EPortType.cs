5aec44259cf1024adec1d7b1452a98114e9cca87
Michael <michaelfiford@gmail.com>
2017-03-03 15:15:15 +0000

    Add alternate client port range

--- Blockchain/Models/EPortType.cs
+++ Blockchain/Models/EPortType.cs
@@ -6,6 +6,10 @@ namespace Blockchain.Models
     public enum EPortType
     {
         MultichainD,
+        // Strictly this doesn't have to be a different range, however there seems to
+        // be an issue running both client and server on the same machine, the client
+        // can't detect that the port is in use by the server. This is a workaround
+        ClientMultichainD,
         Rpc
     }
 
@@ -36,6 +40,9 @@ namespace Blockchain.Models
             {
                 case EPortType.MultichainD:
                     port = 7211;
+                    break;
+                case EPortType.ClientMultichainD:
+                    port = 8211;
                     break;
                 case EPortType.Rpc:
                     port = 24533;
