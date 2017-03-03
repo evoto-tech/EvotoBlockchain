5b6ffa71927d6b9eccae05ebd3f859043074b15e
Michael <michaelfiford@gmail.com>
2017-03-03 14:11:43 +0000

    Create blockchain question model

---
+++ Blockchain/Models/BlockchainQuestionModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Blockchain.Models
+{
+    public class BlockchainQuestionModel
+    {
+        [DataMember(Name = "question")]
+        public string Question { get; set; }
+
+        [DataMember(Name = "answers")]
+        public List<string> Answers { get; set; }
+    }
+}
